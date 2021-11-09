916d3c5e561aa6c9ba43d71500379f29b6837693
Miguel <64503035+envyfull@users.noreply.github.com>
2021-11-08 22:57:05 -0300

    Delete db.php

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // Enter your host name, database username, password, and database name.
-    // If you have not set database password on localhost then set empty.
-    $con = mysqli_connect("remotemysql.com","2h9cmfaaXT","W2RIHfWNIY","2h9cmfaaXT");
-    // Check connection
-    if (mysqli_connect_errno()){
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    }
-?>
